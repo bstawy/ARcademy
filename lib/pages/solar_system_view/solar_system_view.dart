0c34c6baf1dbc559d067e75aa2729bb9249b0d35
basta <125200508@sha.edu.eg>
2023-12-08 19:41:05 +0200

    add texture's path to every item

--- lib/pages/solar_system_view/solar_system_view.dart
+++ lib/pages/solar_system_view/solar_system_view.dart
@@ -7,17 +7,33 @@ class SolarSystemView extends StatelessWidget {
   const SolarSystemView({super.key});
 
   final List<ItemCard> items = const [
-    ItemCard(title: "Sun", imagePath: "assets/images/sun.png"),
-    ItemCard(title: "Mercury Planet", imagePath: "assets/images/mercury.png"),
-    ItemCard(title: "Earth Planet", imagePath: "assets/images/earth.png"),
-    ItemCard(title: "Jupiter Planet", imagePath: "assets/images/jupiter.png"),
+    ItemCard(
+      title: "Sun",
+      imagePath: "assets/images/sun.png",
+      texturePath: "assets/textures/sun_texture.jpg",
+    ),
+    ItemCard(
+      title: "Mercury Planet",
+      imagePath: "assets/images/mercury.png",
+      texturePath: "assets/textures/mercury_texture.jpg",
+    ),
+    ItemCard(
+      title: "Earth Planet",
+      imagePath: "assets/images/earth.png",
+      texturePath: "assets/textures/earth_texture.jpg",
+    ),
+    ItemCard(
+      title: "Jupiter Planet",
+      imagePath: "assets/images/jupiter.png",
+      texturePath: "assets/textures/jupiter_texture.jpg",
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Solar System"),
+        title: const Text("Solar System"),
       ),
       body: Expanded(
         child: ListView.builder(

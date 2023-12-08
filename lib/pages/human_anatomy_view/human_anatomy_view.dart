1263b10e2f000648a8c037477407eefaf09839a9
basta <125200508@sha.edu.eg>
2023-12-08 13:23:02 +0200

    create Human Anatomy View

---
+++ lib/pages/human_anatomy_view/human_anatomy_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HumanAnatomyView extends StatelessWidget {
+  static const String routeName = "/human-anatomy";
+
+  const HumanAnatomyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

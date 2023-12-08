ff43499e20a49cce715ccefa72a7c2a45043bb38
basta <125200508@sha.edu.eg>
2023-12-08 19:40:26 +0200

    add SplashView and AR View routes and set Splash as initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:ar_cademy/pages/solar_system_view/ar_view.dart';
 import 'package:flutter/material.dart';
 
 import 'core/theme/app_theme.dart';
 import 'pages/home_view/home_view.dart';
 import 'pages/human_anatomy_view/human_anatomy_view.dart';
 import 'pages/solar_system_view/solar_system_view.dart';
+import 'pages/splash_view/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,11 +21,13 @@ class MyApp extends StatelessWidget {
       title: 'ARcademy',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      initialRoute: HomeView.routeName,
+      initialRoute: SplashView.routeName,
       routes: {
+        SplashView.routeName: (context) => const SplashView(),
         HomeView.routeName: (context) => const HomeView(),
         HumanAnatomyView.routeName: (context) => const HumanAnatomyView(),
         SolarSystemView.routeName: (context) => const SolarSystemView(),
+        ArView.routeName: (context) => const ArView(),
       },
     );
   }

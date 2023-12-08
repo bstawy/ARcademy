41b159fe69d3d9f7a91e0102ee68815f92981dd3
basta <125200508@sha.edu.eg>
2023-12-08 13:21:34 +0200

    use routes to navigate between views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme/app_theme.dart';
+import 'pages/home_view/home_view.dart';
+import 'pages/human_anatomy_view/human_anatomy_view.dart';
+import 'pages/solar_system_view/solar_system_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ARcademy',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const Scaffold(),
+      initialRoute: HomeView.routeName,
+      routes: {
+        HomeView.routeName: (context) => const HomeView(),
+        HumanAnatomyView.routeName: (context) => const HumanAnatomyView(),
+        SolarSystemView.routeName: (context) => const SolarSystemView(),
+      },
     );
   }
 }

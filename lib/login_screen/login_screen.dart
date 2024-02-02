98cae6ab79083d3cd2d6e179e118508c4f2a8090
bstawy <125200508@sha.edu.eg>
2024-02-02 02:11:12 +0200

    Create LoginScreenWidget

---
+++ lib/login_screen/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routeName = '/';
+
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

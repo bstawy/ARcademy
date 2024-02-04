6b8e7e483fb1243df4c2ef4ae70dfbc6fe418ec8
bstawy <125200508@sha.edu.eg>
2024-02-04 02:18:13 +0200

    Remove elevation of back button when click

--- lib/core/widgets/custom_back_button.dart
+++ lib/core/widgets/custom_back_button.dart
@@ -11,22 +11,25 @@ class CustomBackButton extends StatelessWidget {
     var theme = Theme.of(context);
 
     return MaterialButton(
-        onPressed: () {
-          onClicked();
-        },
-        height: 48.w,
-        minWidth: 48.w,
-        enableFeedback: false,
-        padding: EdgeInsets.all(12.w),
-        color: theme.colorScheme.onBackground,
-        elevation: 0,
-        animationDuration: const Duration(milliseconds: 50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.r),
-        ),
-        child: Icon(
-          Icons.arrow_back_ios_new_rounded,
-          color: theme.colorScheme.primary,
-        ));
+      onPressed: () {
+        onClicked();
+      },
+      height: 48.w,
+      minWidth: 48.w,
+      padding: EdgeInsets.all(12.w),
+      elevation: 0,
+      highlightElevation: 0,
+      highlightColor: theme.colorScheme.secondary,
+      color: theme.colorScheme.onBackground,
+      enableFeedback: true,
+      animationDuration: const Duration(milliseconds: 50),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.r),
+      ),
+      child: Icon(
+        Icons.arrow_back_ios_new_rounded,
+        color: theme.colorScheme.primary,
+      ),
+    );
   }
 }

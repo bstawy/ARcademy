8128e626a0d9898f7fa2dc904da879d6a703813b
bstawy <125200508@sha.edu.eg>
2024-02-04 17:09:12 +0200

    Initialize Firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/theme/application_theme.dart';
+import 'firebase_options.dart';
 import 'login_screen/login_screen.dart';
 import 'onboarding_screens/onboarding_screens.dart';
 import 'sign_up_screen/sign_up_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 

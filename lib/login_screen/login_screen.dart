7ee51d938b499f2af730d82e79d0b323423814b1
bstawy <125200508@sha.edu.eg>
2024-02-05 03:52:27 +0200

    Add sliver app bar to enhance user scrolling experience

--- lib/login_screen/login_screen.dart
+++ lib/login_screen/login_screen.dart
@@ -22,125 +22,180 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   bool isVisible = false;
 
+  final ScrollController _scrollController = ScrollController();
+  bool _isAppBarPinned = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _isAppBarPinned =
+            _scrollController.offset > 35.h; // Adjust the threshold as needed
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomBackButton(onClicked: () {
-                  // pop all screens until get onboarding screens
-                  Navigator.popUntil(context, ModalRoute.withName('/'));
-                }),
-                SizedBox(height: 32.h),
-                Text(
-                  "Welcome Back",
-                  style: theme.textTheme.displaySmall,
-                ),
-                SizedBox(height: 8.h),
-                Text(
-                  "Welcome Back! Please Enter Your Details.",
-                  style: TextStyle(
-                    color: const Color(0xff828a89),
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w400,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: theme.colorScheme.background,
+            toolbarHeight: 73.h,
+            expandedHeight: 145.h,
+            elevation: 0,
+            scrolledUnderElevation: 0,
+            leadingWidth: 72.w,
+            leading: Padding(
+              padding: EdgeInsets.only(top: 18.h, left: 24.w, bottom: 8.h),
+              child: CustomBackButton(onClicked: () {
+                Navigator.pop(context);
+              }),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              expandedTitleScale: 1,
+              collapseMode: CollapseMode.parallax,
+              titlePadding: _isAppBarPinned
+                  ? EdgeInsets.only(bottom: 15.h)
+                  : EdgeInsets.zero,
+              centerTitle: true,
+              title: Padding(
+                padding: EdgeInsets.only(top: 10.h, bottom: 8.h),
+                child: AnimatedDefaultTextStyle(
+                  style: theme.textTheme.displaySmall!,
+                  curve: Curves.linear,
+                  duration: const Duration(milliseconds: 200),
+                  child: Text(
+                    "Welcome Back",
+                    style: (_isAppBarPinned)
+                        ? theme.textTheme.titleMedium!.copyWith(
+                            color: theme.colorScheme.primary,
+                            fontWeight: FontWeight.w600,
+                          )
+                        : theme.textTheme.displaySmall,
                   ),
                 ),
-                SizedBox(height: 32.h),
-                Form(
-                  key: loginFormKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      CustomTextFormField(
-                        textEditingController: emailController,
-                        labelText: "Email",
-                        title: "Enter Your Email",
-                        obscureText: false,
-                      ),
-                      SizedBox(height: 16.h),
-                      CustomTextFormField(
-                        textEditingController: passwordController,
-                        labelText: "Password",
-                        title: "Enter password",
-                        obscureText: !isVisible,
-                        suffixIcon: InkWell(
-                          onTap: () {
-                            isVisible = !isVisible;
-                            setState(() {});
-                          },
-                          borderRadius: BorderRadius.circular(15.r),
-                          child: (isVisible)
-                              ? const Icon(Icons.visibility_off_outlined)
-                              : const Icon(Icons.visibility_outlined),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Forgot Password?",
-                            style: theme.textTheme.labelMedium,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 16.h),
-                      CustomMaterialButton(
-                        title: "Login",
-                        onClicked: () {},
-                      ),
-                      SizedBox(height: 24.h),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SocialMediaAuthButton(
-                              label: "Google",
-                              iconPath: "assets/icons/google_icon.svg",
-                              onClicked: () {}),
-                          SizedBox(width: 8.w),
-                          SocialMediaAuthButton(
-                              label: "Apple",
-                              iconPath: "assets/icons/apple_icon.svg",
-                              onClicked: () {}),
-                        ],
-                      ),
-                      SizedBox(height: 16.h),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Don't have an account?",
-                            style: theme.textTheme.labelLarge!.copyWith(
-                              color: theme.colorScheme.secondary,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                  context, SignUpScreen.routeName);
-                            },
-                            child: Text(
-                              "Sign Up for free",
-                              style: theme.textTheme.labelLarge,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome Back! Please Enter Your Details",
+                    style: TextStyle(
+                      color: const Color(0xff828a89),
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(height: 32.h),
+                  Form(
+                    key: loginFormKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomTextFormField(
+                          textEditingController: emailController,
+                          labelText: "Email",
+                          title: "Enter Your Email",
+                          obscureText: false,
+                        ),
+                        SizedBox(height: 16.h),
+                        CustomTextFormField(
+                          textEditingController: passwordController,
+                          labelText: "Password",
+                          title: "Enter password",
+                          obscureText: !isVisible,
+                          suffixIcon: InkWell(
+                            onTap: () {
+                              isVisible = !isVisible;
+                              setState(() {});
+                            },
+                            borderRadius: BorderRadius.circular(15.r),
+                            child: (isVisible)
+                                ? const Icon(Icons.visibility_off_outlined)
+                                : const Icon(Icons.visibility_outlined),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot Password?",
+                              style: theme.textTheme.labelMedium,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.h),
+                        CustomMaterialButton(
+                          title: "Login",
+                          onClicked: () {},
+                        ),
+                        SizedBox(height: 24.h),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SocialMediaAuthButton(
+                                label: "Google",
+                                iconPath: "assets/icons/google_icon.svg",
+                                onClicked: () {}),
+                            SizedBox(width: 8.w),
+                            SocialMediaAuthButton(
+                                label: "Apple",
+                                iconPath: "assets/icons/apple_icon.svg",
+                                onClicked: () {}),
+                          ],
+                        ),
+                        SizedBox(height: 16.h),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: theme.textTheme.labelLarge!.copyWith(
+                                color: theme.colorScheme.secondary,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, SignUpScreen.routeName);
+                              },
+                              child: Text(
+                                "Sign Up for free",
+                                style: theme.textTheme.labelLarge,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

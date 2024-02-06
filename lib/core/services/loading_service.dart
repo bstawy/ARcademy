077d3c900b13e581a92f12842524bed63c3be87b
bstawy <125200508@sha.edu.eg>
2024-02-06 15:56:50 +0200

    Use screenUtil Package to make responsive loading UI

--- lib/core/services/loading_service.dart
+++ lib/core/services/loading_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../theme/application_theme.dart';
 
@@ -7,9 +8,9 @@ void configureEasyLoading(BuildContext context) {
   var theme = Theme.of(context);
   EasyLoading.instance
     ..indicatorType = EasyLoadingIndicatorType.ring
-    ..indicatorSize = 50.0
-    ..radius = 25.0
-    ..contentPadding = const EdgeInsets.all(25)
+    ..indicatorSize = 50.0.r
+    ..radius = 25.0.r
+    ..contentPadding = EdgeInsets.all(25.r)
     ..loadingStyle = EasyLoadingStyle.custom
     ..backgroundColor = theme.colorScheme.onBackground
     ..indicatorColor = ApplicationTheme.primaryColor

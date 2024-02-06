2c0dda8519d08f3613fb0d3e8a6f2d178e0bae60
bstawy <125200508@sha.edu.eg>
2024-02-07 00:37:23 +0200

    Use CustomSliverAppBar Use CustomLoginForm Fix imports

---
+++ lib/screens/registration_screens/login_screen/login_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../widgets/custom_sliver_app_bar_widget.dart';
+import 'widgets/custom_login_form_widget.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String routeName = '/login';
+
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late ScrollController _scrollController;
+  bool _isAppBarPinned = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      setState(() {
+        _isAppBarPinned =
+            _scrollController.offset > 35.h; // Adjust the threshold as needed
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          CustomSliverAppBarWidget(
+            title: "Welcome Back",
+            isAppBarPinned: _isAppBarPinned,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome Back! Please Enter Your Details",
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  SizedBox(height: 32.h),
+                  const CustomLoginFormWidget(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

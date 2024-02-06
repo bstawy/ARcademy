4086357ea02a35b7cee8bf6f636ad770672f5453
bstawy <125200508@sha.edu.eg>
2024-02-07 00:32:17 +0200

    Custom widget shows user info and change theme button

---
+++ lib/screens/home_screen/widgets/app_bar_title_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'change_theme_button.dart';
+
+class AppBarTitleWidget extends StatelessWidget {
+  const AppBarTitleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Padding(
+      padding: EdgeInsets.only(top: 51.h, left: 24.w, bottom: 8.h, right: 24.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            height: 44.h,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Profile icon
+                Container(
+                  width: 44.w,
+                  height: 44.h,
+                  padding: EdgeInsets.all(10.r),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.r),
+                    color: theme.colorScheme.background,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 20,
+                        spreadRadius: 0,
+                        color: theme.colorScheme.primary.withOpacity(0.05),
+                      ),
+                    ],
+                  ),
+                  child: SvgPicture.asset("assets/icons/nav_profile_icon.svg"),
+                ),
+                SizedBox(width: 16.w),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Mohamed Bastawy",
+                      style: theme.textTheme.labelLarge!.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      "bastawiie@gmail.com",
+                      style: theme.textTheme.labelSmall,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const ChangeThemeButton(),
+        ],
+      ),
+    );
+  }
+}

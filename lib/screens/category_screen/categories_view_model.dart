fe4732a43a935e4b294768c785b81d8568cb6962
bstawy <125200508@sha.edu.eg>
2024-02-13 21:46:45 +0200

    Create categories view model

---
+++ lib/screens/category_screen/categories_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../data/data.dart';
+import '../home_screen/widgets/item_card.dart';
+
+class CategoriesViewModel extends ChangeNotifier {
+  final List<List<Widget>> _items = [
+    SystemsData.systemsOrgans["Cardiovascular System"]!
+        .map((organ) => ItemCard(organ: organ))
+        .toList(),
+    SystemsData.systemsOrgans["Digestive System"]!
+        .map((organ) => ItemCard(organ: organ))
+        .toList(),
+    SystemsData.systemsOrgans["Nervous System"]!
+        .map((organ) => ItemCard(organ: organ))
+        .toList(),
+    SystemsData.systemsOrgans["Respiratory System"]!
+        .map((organ) => ItemCard(organ: organ))
+        .toList(),
+    SystemsData.systemsOrgans["Skeleton System"]!
+        .map((organ) => ItemCard(organ: organ))
+        .toList(),
+  ];
+  final _tabs = const [
+    Tab(text: 'Cardiovascular'),
+    Tab(text: 'Digestive'),
+    Tab(text: 'Nervous'),
+    Tab(text: 'Respiratory'),
+    Tab(text: 'Skeleton'),
+  ];
+  int _selectedItemIndex = 0;
+
+  List<List<Widget>> get items => _items;
+
+  List<Tab> get tabs => _tabs;
+
+  int get selectedItemIndex => _selectedItemIndex;
+
+  changeCurrentItemIndex(int index) {
+    _selectedItemIndex = index;
+    notifyListeners();
+  }
+}

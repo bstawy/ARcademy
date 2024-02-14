721bae04d4d97a7f86a991cf1f8242241f956300
bstawy <125200508@sha.edu.eg>
2024-02-14 05:14:52 +0200

    Add attribute for more customization

--- lib/core/widgets/favorite_button.dart
+++ lib/core/widgets/favorite_button.dart
@@ -2,12 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+// ignore: must_be_immutable
 class FavoriteButton extends StatefulWidget {
+  TextStyle? textStyle;
+  double? height;
+  double? minWidth;
+  double? padding;
+  double? borderRadius;
+  final double iconHeight;
+  final double iconWidth;
+  Color? backgroundColor;
+  Color? highlightColor;
   final bool isFavorite;
   final Function onClicked;
 
-  const FavoriteButton(
-      {super.key, required this.isFavorite, required this.onClicked});
+  FavoriteButton({
+    super.key,
+    this.textStyle,
+    this.height,
+    this.minWidth,
+    this.padding,
+    this.borderRadius,
+    required this.iconWidth,
+    required this.iconHeight,
+    this.backgroundColor,
+    this.highlightColor,
+    required this.isFavorite,
+    required this.onClicked,
+  });
 
   @override
   State<FavoriteButton> createState() => _FavoriteButtonState();
@@ -22,28 +44,34 @@ class _FavoriteButtonState extends State<FavoriteButton> {
       onPressed: () async {
         widget.onClicked();
       },
-      height: 40.h,
-      minWidth: 40.w,
-      padding: EdgeInsets.all(8.w),
+      height: widget.height ?? 40.h,
+      minWidth: widget.minWidth ?? 40.w,
+      padding: EdgeInsets.all(widget.padding ?? 8.r),
       elevation: 0,
       highlightElevation: 0,
-      highlightColor: theme.colorScheme.secondary,
-      color: theme.colorScheme.background,
+      highlightColor: widget.highlightColor ?? theme.colorScheme.secondary,
+      color: widget.backgroundColor ?? theme.colorScheme.background,
       enableFeedback: true,
       animationDuration: const Duration(milliseconds: 50),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.r),
+        borderRadius: BorderRadius.circular(widget.borderRadius ?? 15.r),
       ),
       child: (widget.isFavorite)
           ? SvgPicture.asset(
               "assets/icons/favorite_filled_icon.svg",
               colorFilter:
                   ColorFilter.mode(theme.colorScheme.primary, BlendMode.srcIn),
+              width: widget.iconWidth,
+              height: widget.iconHeight,
+              fit: BoxFit.contain,
             )
           : SvgPicture.asset(
-              "assets/icons/favorite_icon.svg",
+        "assets/icons/favorite_icon.svg",
               colorFilter:
                   ColorFilter.mode(theme.colorScheme.primary, BlendMode.srcIn),
+              width: widget.iconWidth,
+              height: widget.iconHeight,
+              fit: BoxFit.contain,
             ),
     );
   }

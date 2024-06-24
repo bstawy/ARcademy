1f9e44fda3de436622022e1246f45bbc918fc512
bastawy <125200508@sha.edu.eg>
2024-06-24 16:31:13 +0300

    Add text to unity view

--- lib/screens/ar_view_screen/ar_view_screen.dart
+++ lib/screens/ar_view_screen/ar_view_screen.dart
@@ -17,6 +17,22 @@ class _ArViewScreenState extends State<ArViewScreen> {
   int? organId;
   bool isReady = false;
 
+  Map<int, String> organsDetails = {
+    1: """The heart is a vital organ that pumps blood throughout the body, providing oxygen and nutrients to tissues and organs while removing waste products.
+Chambers: Divided into four chambers - two atria and two ventricles - which receive and pump blood.\n
+Valves: Ensure unidirectional blood flow within the heart.\n
+Coronary Arteries: Supply oxygenated blood to the heart muscle.""",
+    20: """The digestive system processes food, extracts nutrients, and eliminates waste. It includes organs such as the mouth, stomach, small intestine, and large intestine.
+Mouth: Begins mechanical and chemical digestion of food.\n
+Stomach: Stores and breaks down food with gastric juices.\n
+Small Intestine: Absorbs nutrients from digested food.\n
+Large Intestine: Absorbs water and forms feces for elimination.""",
+    41: """The brain is the command center of the nervous system, responsible for processing sensory information, coordinating motor responses, and regulating basic bodily functions.
+Cerebrum: Largest part of the brain, responsible for higher cognitive functions such as thinking and memory.\n
+Cerebellum: Located below the cerebrum, involved in coordination and balance.\n
+Brainstem: Connects the brain to the spinal cord and regulates basic functions like breathing and heartbeat.""",
+  };
+
   @override
   void initState() {
     super.initState();
@@ -44,6 +60,20 @@ class _ArViewScreenState extends State<ArViewScreen> {
                 },
                 child: Stack(
                   children: [
+                    Visibility(
+                      visible: isReady,
+                      child: Positioned(
+                        child: Container(
+                          padding: EdgeInsets.all(16.w),
+                          margin: EdgeInsets.only(top: 16.h, left: 16.w),
+                          width: 50.w,
+                          child: Text(
+                            organsDetails[organId] ?? '',
+                            style: Theme.of(context).textTheme.labelLarge,
+                          ),
+                        ),
+                      ),
+                    ),
                     UnityWidget(
                       onUnityCreated: onUnityCreated,
                       fullscreen: false,
